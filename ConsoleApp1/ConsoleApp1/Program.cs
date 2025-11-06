19b7fa0fde6cc145a2aee7337acbb0f690fbf704
ali g.baee <140052913+Ali0500@users.noreply.github.com>
2025-11-06 20:43:18 +0330

    save 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine(
+    """
+    This is a raw string literal in C# 13.
+    It can span multiple lines
+    and include "quotes" without needing to escape them.
+    """
+);
